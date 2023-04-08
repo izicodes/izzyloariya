c0aa87cfb40aa7373f3617d54ab2aa35ff0b4b29
Isabelle <loariyafiaba@gmail.com>
2023-04-08 17:03:28 +0100

    Added the new files and code

--- src/script.js
+++ src/script.js
@@ -1,49 +1,63 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // BULMA JS CODE!!
+// ------------------------------------ //
+// >> VARIABLES <<
+const button = document.querySelector("#down-icon-box");
+const toggler = document.querySelector(".toggle-box");
+const togglerIcon = document.querySelector(".toggler-icon");
+const hero = document.querySelector(".hero");
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
+// ------------------------------------ //
+// >> MAIN <<
+button.addEventListener("click", smoothScroll);
 
-  // Add a click event on each of them
-  $navbarBurgers.forEach((el) => {
-    el.addEventListener("click", () => {
-      // Get the target from the "data-target" attribute
-      const target = el.dataset.target;
-      const $target = document.getElementById(target);
+// ------------------------------------ //
+// >> FUNCTIONS <<
+toggler.addEventListener("click", () => {
+  if (toggler.dataset.mode === "light") {
+    nightMode();
+    toggler.dataset.mode = "night";
+  } else {
+    lightMode();
+    toggler.dataset.mode = "light";
+  }
+  feather.replace();
+});
+function nightMode() {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = "var(--black-blue)";
+  toggler.style.borderColor = "white";
+  toggler.style.color = "white";
+  toggler.style.backgroundColor = "black";
+  togglerIcon.classList.remove("yellow-icon");
+  togglerIcon.classList.add("white-icon");
+  togglerIcon.removeAttribute("data-feather");
+  feather.replace();
+}
+function lightMode() {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = "#fdfcdc";
+  toggler.style.borderColor = "black";
+  toggler.style.backgroundColor = "white";
+  togglerIcon.classList.remove("yellow-icon");
+  togglerIcon.classList.add("white-icon");
+  togglerIcon.removeAttribute("data-feather");
+  feather.replace();
+}
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId = event.currentTarget.dataset.target;
+  const targetPosition = document.querySelector(targetId).offsetTop;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 650;
+  let start = null;
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      el.classList.toggle("is-active");
-      $target.classList.toggle("is-active");
-    });
-  });
-
-  // ----------------------------------------- //
-
-  // This is for the scrolling from hero page to the first heading!
-
-  function smoothScroll(event) {
-    event.preventDefault();
-    const targetId = event.currentTarget.dataset.target;
-    const targetPosition = document.querySelector(targetId).offsetTop;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    const duration = 700;
-    let start = null;
-
-    function step(timestamp) {
-      if (!start) start = timestamp;
-      const progress = timestamp - start;
-      const progressPercentage = Math.min(progress / duration, 1);
-      window.scrollTo(0, startPosition + distance * progressPercentage);
-      if (progress < duration) window.requestAnimationFrame(step);
-    }
-
-    window.requestAnimationFrame(step);
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    const progressPercentage = Math.min(progress / duration, 1);
+    window.scrollTo(0, startPosition + distance * progressPercentage);
+    if (progress < duration) window.requestAnimationFrame(step);
   }
 
-  const button = document.querySelector("#go-down-button");
-  button.addEventListener("click", smoothScroll);
-});
+  window.requestAnimationFrame(step);
+}

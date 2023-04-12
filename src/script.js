9f2d020dd03b0d4bfada1d535223d5bbeff25a9c
Isabelle <loariyafiaba@gmail.com>
2023-04-12 23:18:30 +0100

    Changed the toggler event listener function!

--- src/script.js
+++ src/script.js
@@ -15,36 +15,13 @@ button.addEventListener("click", smoothScroll);
 // >> FUNCTIONS <<
 toggler.addEventListener("click", () => {
   if (toggler.dataset.mode === "light") {
-    // nightMode();
     hideElement(navLinksSection, 2);
     toggler.dataset.mode = "night";
   } else {
     hideElement(navLinksSection, 1);
     toggler.dataset.mode = "light";
   }
-  feather.replace();
 });
-function nightMode() {
-  const body = document.querySelector("body");
-  body.style.backgroundColor = "var(--black-blue)";
-  toggler.style.borderColor = "white";
-  toggler.style.color = "white";
-  toggler.style.backgroundColor = "black";
-  togglerIcon.classList.remove("yellow-icon");
-  togglerIcon.classList.add("white-icon");
-  togglerIcon.removeAttribute("data-feather");
-  feather.replace();
-}
-function lightMode() {
-  const body = document.querySelector("body");
-  body.style.backgroundColor = "#fdfcdc";
-  toggler.style.backgroundColor = "white";
-  toggler.style.borderColor = "black";
-  toggler.style.color = "black";
-  togglerIcon.classList.remove("yellow-icon");
-  togglerIcon.removeAttribute("data-feather");
-  feather.replace();
-}
 function smoothScroll(event) {
   event.preventDefault();
   const targetId = event.currentTarget.dataset.target;
